37ce3592590a8dfb10aba2d4200ff379577dd20c
MeganBarrera <>
2019-11-26 02:30:13 -0600

    About

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,21 +7,21 @@ module.exports = (app)=>{
     app.get("/about", (req, res)=>{
         res.render("about")
     });
-    app.get("/portfolio", (req, res)=>{
-        res.render("portfolio")
-    });
-        app.get("/portfolio-categories", (req, res)=>{
-            res.render("portfolio-categories")
+    // app.get("/portfolio", (req, res)=>{
+    //     res.render("portfolio")
+    // });
+        app.get("/experience", (req, res)=>{
+            res.render("experience")
         });
         app.get("/portfolio-details", (req, res)=>{
             res.render("portfolio-details")
         });
-    app.get("/blog", (req, res)=>{
-        res.render("blog")
-    });
-        app.get("/blog-details", (req, res)=>{
-            res.render("blog-details")
-        });
+    // app.get("/blog", (req, res)=>{
+    //     res.render("blog")
+    // });
+    //     app.get("/blog-details", (req, res)=>{
+    //         res.render("blog-details")
+    //     });
     app.get("/contact", (req, res)=>{
         res.render("contact")
     });
